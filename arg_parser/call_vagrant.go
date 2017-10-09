d7a04a400175f5b1859f9e4310023605e4a35cc3
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2017-10-09 12:12:11 -0700

    Add some bare code

---
+++ arg_parser/call_vagrant.go
@@ -0,0 +1,13 @@
+package arg_parser
+
+func createONAPComponent(component string)  {
+	// do vagrant up <component>
+}
+
+func createONAPcomponent_DockerBuild(component string)  {
+	// do vagrant up <component> and do stuff for DockerBuild
+}
+
+func createONAPComponent_DockerRun(component string)  {
+	// do vagrant up <component> and do stuff for DockerRun
+}
